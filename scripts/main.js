302cb8892881eb6ffd690bb616d165628adbfc0a
Podlina <polinka92668@gmail.com>
2024-06-21 11:16:56 +0300

    Переименование indexquiz.html на index.html и stylequiz.css на style.css, убраны комментарии из style.css, написаны комментарии к коду main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    /* ссылки на элементы страницы:
+    - startButton переменная для кнопки начала квиза;
+    -  nextButton переменная для кнопки перехода к следующему вопросу;
+    - checkButton переменная для кнопки проверки ответа на вопрос, 
+    который предпологает несколько вариантов ответа;
+    - questionCounterElement переменная для отображения текущего вопроса и 
+    всего количества вопросов;
+    - resultsElement переменная для оотображения результатов квиза после его окончения;
+    - questionContainerElement пересенная для хранения самих вопросов и вариантов ответа;
+    - questionElement - для отображения текущего вопроса;
+    - answerButtonsElement - для кнопок, содержащих возможные варианты ответа;
+    */
     const startButton = document.getElementById('start-btn');
     const nextButton = document.getElementById('next-btn');
     const checkButton = document.getElementById('check-btn');
@@ -9,26 +21,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const questionElement = document.getElementById('question');
     const answerButtonsElement = document.getElementById('answer-buttons');
 
+    /*
+     Инициализация переменных:
+    - для хранения массива вопросов (shuffledQuestions), 
+    - текущего индекса вопроса в квизе (currentQuestionIndex),
+    - для счетаправильных ответов пользователя (score), 
+    */
     let shuffledQuestions, currentQuestionIndex, score = 0;
 
+    // Добавление обработчка событий для кнопки "Start" 
     startButton.addEventListener('click', startGame);
 
+    //Функция начала квиза
     function startGame() {
+
+        /* Скрытие кнопки начала квиза и элемента, отображающего результат,
+        показ счетчика вопросов и контейнер вопросов*/
         startButton.classList.add('hide');
         questionCounterElement.classList.remove('hide');
         questionContainerElement.classList.remove('hide');
         resultsElement.classList.add('hide');
+        
+        //Перемешивание вопросов и выбор 10-ти первых
         shuffledQuestions = getRandomQuestions(questions, 10);
         currentQuestionIndex = 0;
         score = 0;
+
+        //
         setNextQuestion();
     }
 
+    //Функция для получения случайных вопросов
     function getRandomQuestions(questions, count) {
         const shuffled = questions.sort(() => Math.random() - 0.5);
         return shuffled.slice(0, Math.min(count, questions.length));
     }
 
+    //Функция установки слкдующего вопроса
     function setNextQuestion() {
         resetState();
         if (currentQuestionIndex < shuffledQuestions.length) {
@@ -39,14 +68,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //Функция показа вопроса
     function showQuestion(question) {
         questionElement.innerText = question.question;
+
+        //Если вопрос предпологает изображение, то добавляем его
         if (question.image) {
             const imageElement = document.createElement('img');
             imageElement.src = question.image;
             imageElement.classList.add('question-image');
             questionElement.appendChild(imageElement);
         }
+
+        //Создание кнопок с вариантами ответа на вопрос и добавление обработчика событий
         question.answers.forEach(answer => {
             const button = document.createElement('button');
             button.innerText = answer.text;
@@ -59,6 +93,7 @@ document.addEventListener('DOMContentLoaded', () => {
             answerButtonsElement.appendChild(button);
         });
 
+        //Показ или скрытие кнопки проверки для вопросов с несколькими вариантами ответа
         if (question.multiple) {
             checkButton.classList.remove('hide');
         } else {
@@ -66,6 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //Функция для сброса сосотяния текущего вопроса
     function resetState() {
         nextButton.classList.add('hide');
         checkButton.classList.add('hide');
@@ -77,6 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    //Функция выбора варианта ответа
     function selectAnswer(selectedButton, question) {
         if (question.multiple) {
             selectedButton.classList.toggle('selected');
@@ -94,15 +131,18 @@ document.addEventListener('DOMContentLoaded', () => {
         nextButton.classList.remove('hide');
     }
 
+    //Добавление обработчика событий для кнопки, проверяющей ответы 
     checkButton.addEventListener('click', () => {
         handleMultipleChoiceAnswer();
     });
 
+    //Добавление обработчика событий для кнопки переходя к следующему событию
     nextButton.addEventListener('click', () => {
         currentQuestionIndex++;
         setNextQuestion();
     });
 
+    //Функция обработки ответов на вопросы, предпологающие насколько вариантов ответа
     function handleMultipleChoiceAnswer() {
         const selectedButtons = document.querySelectorAll('.multiple-choice.selected');
         const allButtons = Array.from(answerButtonsElement.children);
@@ -126,6 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
         nextButton.classList.remove('hide');
     }
 
+    //// Функции для установки и очистки классов статуса ответа
     function setStatusClass(element, correct) {
         clearStatusClass(element);
         if (correct) {
@@ -141,10 +182,12 @@ document.addEventListener('DOMContentLoaded', () => {
         element.classList.remove('selected');
     }
 
+    //Функция обновления счётчика вопросов
     function updateQuestionCounter() {
         questionCounterElement.innerText = `Question ${currentQuestionIndex + 1} out of ${shuffledQuestions.length}`;
     }
 
+    //Функция завершения квиза, показа результатов и повторного прохождения квиза
     function concludeQuiz() {
         questionContainerElement.classList.add('hide');
         nextButton.classList.add('hide');
