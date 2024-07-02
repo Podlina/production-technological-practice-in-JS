b055f63935da03399da84c85335e6f2c886eeb5f
Polina <120045125+Podlina@users.noreply.github.com>
2024-07-02 18:09:22 +0300

    Delete Module/JS_Core_Advanced directory

--- Module/JS_Core_Advanced/task_v1_n4.js
+++
@@ -1,8 +0,0 @@
-function stature(arr){
-    for (let i = 0; i<arr.length; i++) {
-        arr[i] += (arr[i] * 0.1);
-        console.log(arr[i]);
-    }
-
-}
-stature([100, 200, 300, 400]);
--- Module/JS_Core_Advanced/task_v1_n5.js
+++
@@ -1,6 +0,0 @@
-function sayHi (){
-    console.log('Hello Vitebsk!');
-}
-
- const intervalID = setInterval(sayHi, 1000);
- setTimeout(() => { clearInterval(intervalID); console.log('stop'); }, 5000);
